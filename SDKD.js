020cbf88371cc997f48709aa08bd76d4dcd0918e
Chris Cassano <glitch0@gmail.com>
2017-10-17 15:33:23 -0700

    hurray, non-hacky wallet generation

--- SDKD.js
+++ SDKD.js
@@ -4,10 +4,10 @@ export default class SDKD {
   constructor(apiKey){
     this.apiKey = apiKey;
     console.log('[SDKD]: constructor()');
-    let w = Wallet.generate();
-    console.log('wallet address: '+w.getAddressString());
   }
   generateWallet(email){
     console.log('[SDKD]: generateWallet('+email+')');
+    this.wallet = Wallet.generate();
+    console.log('wallet address: '+this.wallet.getAddressString());
   }
 }
