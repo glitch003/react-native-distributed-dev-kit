66611a593f27908ec96d933ce9b2185be5cf979f
Chris Cassano <glitch0@gmail.com>
2017-10-30 13:47:13 -0700

    tests started, snapshot created

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,24 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
+
+jest.mock('WebView', () => 'WebView')
+
+jest.mock('NativeModules', () => {
+  return {
+    RNRandomBytes: {
+      seed: undefined,
+      randomBytes: jest.fn()
+    }
+  }
+})
 
 it('renders correctly', () => {
   const tree = renderer.create(
     <App />
-  );
-});
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
